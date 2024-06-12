22742c3445c67091f2d3ccca6390157cc1a81439
CodeStarter40 <bhgzpro@pm.me>
2024-06-12 16:54:20 +0200

    modify CandidatDto

--- app/src/main/java/com/openclassrooms/vitesse/data/entity/CandidatDto.kt
+++ app/src/main/java/com/openclassrooms/vitesse/data/entity/CandidatDto.kt
@@ -1,5 +1,6 @@
 package com.openclassrooms.vitesse.data.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.security.KeyStore.TrustedCertificateEntry
@@ -7,15 +8,35 @@ import java.util.Date
 
 @Entity(tableName = "candidat")
 data class CandidatDto (
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long = 0,
+
+    @ColumnInfo(name = "nom")
     val nom: String,
+
+    @ColumnInfo(name = "prenom")
     val prenom: String,
+
+    @ColumnInfo(name = "dateBirth")
     val dateBirth: Date,
-    val photo: String,
+
+    @ColumnInfo(name = "picture")
+    val picture: String,
+
+    @ColumnInfo(name = "email")
     val email:String,
+
+    @ColumnInfo(name = "note")
     val note:String,
+
+    @ColumnInfo(name = "pretend")
     val pretend:Double,
+
+    @ColumnInfo(name = "favori")
     val favori:Boolean,
+
+    @ColumnInfo(name = "phone")
     val phone:String,
 
 )
--- app/src/main/java/com/openclassrooms/vitesse/data/repository/CandidatsRepository.kt
+++ app/src/main/java/com/openclassrooms/vitesse/data/repository/CandidatsRepository.kt
@@ -1,9 +1,10 @@
 package com.openclassrooms.vitesse.data.repository
 
-import com.openclassrooms.vitesse.data.entity.CandidatDto
+
 import com.openclassrooms.vitesse.data.dao.CandidatDtoDao
+import com.openclassrooms.vitesse.data.entity.CandidatDto
 import kotlinx.coroutines.flow.Flow
 
-class CandidatsRepository(private val candidatDtoDao: CandidatDtoDao) {
-    val allCandidats: Flow<List<CandidatDto>> = candidatDtoDao.getAllCandidat()
+class CandidatsRepository(private val candidatDao: CandidatDtoDao) {
+    val allCandidats: Flow<List<CandidatDto>> = candidatDao.getAllCandidat()
 }
